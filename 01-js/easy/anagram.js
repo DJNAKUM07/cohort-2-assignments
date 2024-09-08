edf530ebc2d2b3556da678b5b0cf17e8340ab3c0
Digvijay Nakum <190540107141@darshan.ac.in>
2024-09-08 15:58:57 +0530

    test commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  // Check if the lengths are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  // Convert the strings to arrays and sort them
+  const str1Arr = str1.toLowerCase().split('').sort();
+  const str2Arr = str2.toLowerCase().split('').sort();
+
+  // Check if the arrays are equal
+  return str1Arr.join('') === str2Arr.join('');
 }
 
 module.exports = isAnagram;
